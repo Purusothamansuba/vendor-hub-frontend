33d688e60b90fdd794a2fe00f25785fe605edf0b
purushothamansubashree <103053707+Purusothamansuba@users.noreply.github.com>
2026-05-26 09:25:34 +0530

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,49 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import ScrollToTop from "./components/common/ScrollToTop";
 // Layout imports
-import BuyerLayout from './layouts/BuyerLayout';
-import SellerLayout from './layouts/SellerLayout';
-import AdminLayout from './layouts/AdminLayout';
-import AuthLayout from './layouts/AuthLayout';
+import BuyerLayout from "./layouts/BuyerLayout";
+import SellerLayout from "./layouts/SellerLayout";
+import AdminLayout from "./layouts/AdminLayout";
+import AuthLayout from "./layouts/AuthLayout";
 
 // Auth page imports
-import RoleSelection from './pages/auth/RoleSelection';
-import Login from './pages/auth/Login';
-import Register from './pages/auth/Register';
-import ForgotPassword from './pages/auth/ForgotPassword';
+import RoleSelection from "./pages/auth/RoleSelection";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import ForgotPassword from "./pages/auth/ForgotPassword";
 
 // Buyer page imports
-import Intro from './pages/buyer/Intro';
-import Home from './pages/buyer/Home';
-import ProductListing from './pages/buyer/ProductListing';
-import ProductDetails from './pages/buyer/ProductDetails';
-import SmartSearch from './pages/buyer/SmartSearch';
-import Cart from './pages/buyer/Cart';
-import Checkout from './pages/buyer/Checkout';
-import RazorpayPayment from './pages/buyer/RazorpayPayment';
-import OrderSuccess from './pages/buyer/OrderSuccess';
-import Orders from './pages/buyer/Orders';
-import OrderTracking from './pages/buyer/OrderTracking';
-import Profile from './pages/buyer/Profile';
+import Intro from "./pages/buyer/Intro";
+import Home from "./pages/buyer/Home";
+import ProductListing from "./pages/buyer/ProductListing";
+import ProductDetails from "./pages/buyer/ProductDetails";
+import SmartSearch from "./pages/buyer/SmartSearch";
+import Cart from "./pages/buyer/Cart";
+import Checkout from "./pages/buyer/Checkout";
+import RazorpayPayment from "./pages/buyer/RazorpayPayment";
+import OrderSuccess from "./pages/buyer/OrderSuccess";
+import Orders from "./pages/buyer/Orders";
+import OrderTracking from "./pages/buyer/OrderTracking";
+import Profile from "./pages/buyer/Profile";
 
 // Seller page imports
-import SellerDashboard from './pages/seller/SellerDashboard';
-import ManageProducts from './pages/seller/ManageProducts';
-import AddProduct from './pages/seller/AddProduct';
-import EditProduct from './pages/seller/EditProduct';
-import InventoryStock from './pages/seller/InventoryStock';
-import EarningsDashboard from './pages/seller/EarningsDashboard';
-import SellerOrders from './pages/seller/SellerOrders';
+import SellerDashboard from "./pages/seller/SellerDashboard";
+import ManageProducts from "./pages/seller/ManageProducts";
+import AddProduct from "./pages/seller/AddProduct";
+import EditProduct from "./pages/seller/EditProduct";
+import InventoryStock from "./pages/seller/InventoryStock";
+import EarningsDashboard from "./pages/seller/EarningsDashboard";
+import SellerOrders from "./pages/seller/SellerOrders";
 
 // Admin page imports
-import AdminDashboard from './pages/admin/AdminDashboard';
-import VendorApproval from './pages/admin/VendorApproval';
-import RefundManagement from './pages/admin/RefundManagement';
-import CommissionSettings from './pages/admin/CommissionSettings';
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import VendorApproval from "./pages/admin/VendorApproval";
+import RefundManagement from "./pages/admin/RefundManagement";
+import CommissionSettings from "./pages/admin/CommissionSettings";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         {/* Auth Route Segment */}
         <Route element={<AuthLayout />}>
@@ -91,10 +91,10 @@ function App() {
           <Route path="/admin/commissions" element={<CommissionSettings />} />
         </Route>
 
-        {/* Fallback Redirects */}
+        {/* Fallback Redirect */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
